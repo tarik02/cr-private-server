cda3a2f0f6c85784d6d9ec15ce3118faf5967fd4
Xset <xsetthe@gmail.com>
2017-06-01 15:41:49 +0300

    Fixed bug when you create a clan.

--- src/main/java/royaleserver/protocol/messages/command/JoinClan.java
+++ src/main/java/royaleserver/protocol/messages/command/JoinClan.java
@@ -11,7 +11,7 @@ public class JoinClan extends Command {
 	public long allianceId;
 	public String name;
 	public SCID badge;
-	public byte unknown_0;
+	public byte unknown_3;
 	public byte accepted;
 
 	public JoinClan() {
@@ -20,8 +20,19 @@ public class JoinClan extends Command {
 		allianceId = 0;
 		name = "";
 		badge = new SCID(16, 0);
-		unknown_0 = 0;
+		unknown_3 = 0;
 		accepted = 0;
+	}
+
+	public static JoinClan from(ClanEntity entity) {
+		JoinClan joinClan = new JoinClan();
+		joinClan.allianceId = entity.getId();
+		joinClan.name = entity.getName();
+		joinClan.badge = entity.getLogicBadge().getScid();
+		joinClan.unknown_3 = 1;
+		joinClan.accepted = 1;
+
+		return joinClan;
 	}
 
 	@Override
@@ -31,18 +42,7 @@ public class JoinClan extends Command {
 		stream.putBLong(allianceId);
 		stream.putString(name);
 		stream.putSCID(badge);
-		stream.putByte(unknown_0);
+		stream.putByte(unknown_3);
 		stream.putByte(accepted);
-	}
-
-	public static JoinClan from(ClanEntity entity) {
-		JoinClan joinClan = new JoinClan();
-		joinClan.allianceId = entity.getId();
-		joinClan.name = entity.getName();
-		joinClan.badge = entity.getLogicBadge().getScid();
-		joinClan.unknown_0 = 0;
-		joinClan.accepted = 1;
-
-		return joinClan;
 	}
 }
