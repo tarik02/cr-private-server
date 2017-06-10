92d7d3d97434202ba1373de991a60f0938982ff2
Tarik02 <Taras.Fomin@gmail.com>
2017-06-10 11:23:54 +0300

    Remove kotlin dependency.

---
+++ src/main/java/royaleserver/utils/Tag.java
@@ -0,0 +1,74 @@
+package royaleserver.utils;
+
+public final class Tag {
+	private static final String TAG_CHARACTERS = "0289PYLQGRJCUV";
+	private final long id;
+	private String string = null;
+
+	public Tag(long id) {
+		this.id = id;
+	}
+
+	public Tag(String tag) {
+		long parsedId = 0;
+
+		for (char c : tag.toCharArray()) {
+			int index = TAG_CHARACTERS.indexOf(c);
+			parsedId *= TAG_CHARACTERS.length();
+			parsedId += index;
+		}
+
+		final long high = parsedId % 256;
+		final long low = (parsedId - high) >>> 8;
+
+		id = (high << 32) | low;
+	}
+
+	public long id() {
+		return id;
+	}
+
+	public String string() {
+		if (string != null) {
+			return string;
+		}
+
+		final long high = (id >> 32) & 0xFFFFFFFF;
+		final long low = id & 0xFFFFFFFF;
+
+		long parsedId = (low << 8) + high;
+		final StringBuilder sb = new StringBuilder();
+
+		while (parsedId != 0L) {
+			int index = (int)(parsedId % TAG_CHARACTERS.length());
+			sb.append(TAG_CHARACTERS.charAt(index));
+			parsedId /= TAG_CHARACTERS.length();
+		}
+
+		return string = sb.toString();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Tag)) {
+			return false;
+		}
+
+		Tag tag = (Tag)o;
+
+		return id == tag.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return (int)(id ^ (id >>> 32));
+	}
+
+	@Override
+	public String toString() {
+		return string;
+	}
+}
